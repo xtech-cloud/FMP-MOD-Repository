f045c955215603dcf9290b43355bd1ea9ad979cf
Easlee Liu <easlee@outlook.com>
2022-08-22 13:24:24 +0800

    generated

---
+++ unity2021/Repository/Assets/Scripts/Module/MySubject.cs
@@ -0,0 +1,7 @@
+
+namespace XTC.FMP.MOD.Repository.LIB.Unity
+{
+    public class MySubject : MySubjectBase
+    {
+    }
+}
---
+++ vs2022/fmp-xtc-repository-lib-mvcs/Subjects.cs
@@ -0,0 +1,16 @@
+
+namespace XTC.FMP.MOD.Repository.LIB.MVCS
+{
+    public class Subjects
+    {
+        /// <summary>
+        /// 示例
+        /// </summary>
+        /// <example>
+        /// var data = new Dictionary<string, object>();
+        /// data["uid"] = "default";
+        /// model.Publish(/XTC/Repository/Sample, data);
+        /// </example>
+        public const string Sample = "/XTC/Repository/Sample";
+    }
+}
