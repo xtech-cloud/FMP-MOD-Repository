cb654f8aef27b3d8e22f48a99c38b40a39f33206
Easlee Liu <easlee@outlook.com>
2022-09-27 22:32:48 +0800

    support permission

---
+++ vs2022/fmp-xtc-repository-lib-mvcs/Permissions.cs
@@ -0,0 +1,28 @@
+
+namespace XTC.FMP.MOD.Repository.LIB.MVCS
+{
+    public class Permissions
+    {
+
+        public const string AgentCreate = "/XTC/Repository/Agent/Create";
+        public const string AgentUpdate = "/XTC/Repository/Agent/Update";
+        public const string AgentRetrieve = "/XTC/Repository/Agent/Retrieve";
+        public const string AgentDelete = "/XTC/Repository/Agent/Delete";
+
+        public const string HealthyCreate = "/XTC/Repository/Healthy/Create";
+        public const string HealthyUpdate = "/XTC/Repository/Healthy/Update";
+        public const string HealthyRetrieve = "/XTC/Repository/Healthy/Retrieve";
+        public const string HealthyDelete = "/XTC/Repository/Healthy/Delete";
+
+        public const string ModuleCreate = "/XTC/Repository/Module/Create";
+        public const string ModuleUpdate = "/XTC/Repository/Module/Update";
+        public const string ModuleRetrieve = "/XTC/Repository/Module/Retrieve";
+        public const string ModuleDelete = "/XTC/Repository/Module/Delete";
+
+        public const string PluginCreate = "/XTC/Repository/Plugin/Create";
+        public const string PluginUpdate = "/XTC/Repository/Plugin/Update";
+        public const string PluginRetrieve = "/XTC/Repository/Plugin/Retrieve";
+        public const string PluginDelete = "/XTC/Repository/Plugin/Delete";
+
+    }
+}
